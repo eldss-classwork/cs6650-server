edab7f08db0e03af9e2804c28ef9a4247e144460
Evan <EvanLDouglass@gmail.com>
2020-09-19 23:49:55 -0700

    Add ResortServlet logic

--- src/main/java/ResortServlet.java
+++ src/main/java/ResortServlet.java
@@ -1,4 +1,3 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -9,15 +8,46 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ResortServlet")
 public class ResortServlet extends HttpServlet {
-  protected void doPost(HttpServletRequest request,
-      HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setContentType("application/json;charset=UTF-8");
-  }
 
   protected void doGet(HttpServletRequest request,
       HttpServletResponse response)
       throws ServletException, IOException {
-    response.setContentType("application/json;charset=UTF-8");
+    String[] pathParts;
+    try {
+      pathParts = ServletMethodSetup.setUp(request, response);
+    } catch (EmptyPathException e) {
+      // Error response taken care of in setup
+      return;
+    }
+
+    if (!isUrlValidGET(pathParts)) {
+      // Invalid path
+      response.sendError(HttpServletResponse.SC_NOT_FOUND, "unknown path provided");
+    } else {
+      response.setStatus(HttpServletResponse.SC_OK);
+
+      // Echo back the path and anything in the query
+      try (PrintWriter out = response.getWriter()) {
+        out.println("Path: " + request.getPathInfo());
+        out.println("Query: " + request.getQueryString());
+      }
+    }
+  }
+
+  /**
+   * Checks if a POST request path is valid.
+   * @param pathParts An array of path parts
+   * @return true if valid, otherwise false
+   */
+  private boolean isUrlValidGET(String[] pathParts) {
+    // urlPath  = "/day/top10vert"
+    // pathParts = ["", "day", "top10vert"]
+    final String resource1 = "day";
+    final String resource2 = "top10vert";
+    // Ignores path params past valid part
+    if (pathParts.length >= 3) {
+      return pathParts[1].equals(resource1) && pathParts[2].equals(resource2);
+    }
+    return false;
   }
 }
